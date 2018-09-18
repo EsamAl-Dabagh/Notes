e7bb699bad0e0ef61faa6df57a19629a9f7361d5
Esam Al-Dabagh <esam.aldabagh@gmail.com>
2018-09-18 13:55:02 +0100

    spike - showing notes on index.html

--- src/note-controller.js
+++ src/note-controller.js
@@ -1,11 +1,22 @@
-
 (function(exports) {
-  
-  function changeText(id, newText) {
-    var divText = document.getElementById(id);
-    divText.innerHTML = newText;
-  }
-  
+  function NoteController(notebook) {
+    this.notebook = new notebook();
+  }  
 
-  exports.changeText = changeText;
+  NoteController.prototype.addNote = function(text) {
+    this.notebook.createNote(text);
+    this.createView();
+  }
+
+  NoteController.prototype.createView = function() {
+    var view = new NotebookView(this.notebook);
+
+    var htmlString = view.returnHtml();
+
+    var element = document.getElementById("app");
+
+    element.innerHTML = htmlString;
+  }
+
+  exports.NoteController = NoteController;
 })(this);
